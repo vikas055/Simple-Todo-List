7ac0ce13d8147cd8019e7329149287700ecaccb4
vikas055 <vikasnaidupadala123@gmail.com>
2022-09-24 11:22:45 +0000

    Simple Todo List

---
+++ src/components/TodoItem/index.js
@@ -0,0 +1,21 @@
+// Write your code here
+
+const TodoItem = props => {
+  const {todoDetails, deleteTodo} = props
+  const {id, title} = todoDetails
+
+  const onDeleteTodo = () => {
+    deleteTodo(id)
+  }
+
+  return (
+    <li className="todo-item">
+      <p className="title">{title}</p>
+      <button type="button" className="delete-btn" onClick={onDeleteTodo}>
+        Delete
+      </button>
+    </li>
+  )
+}
+
+export default TodoItem
